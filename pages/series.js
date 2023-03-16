3a0309bc1c5b7175ebcc482dc6216f3f33d50da1
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-16 00:14:13 -0500

    basic structure

--- pages/series.js
+++ pages/series.js
@@ -1,6 +1,7 @@
 import Nav from "../components/navigation";
 import MovieList from "../components/ui-components/movie-list";
-
+import { useContext } from "react";
+import { dataContext } from "../context/dataContext";
 const Series = () => {
   const { seriesCategory: data } = useContext(dataContext);
   return (
