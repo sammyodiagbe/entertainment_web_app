44253edf44c15e63f0c15146f7210c69b51ac996
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-16 00:00:27 -0500

    working on structure

--- pages/bookmarked.js
+++ pages/bookmarked.js
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import Nav from "../components/navigation";
+import MovieList from "../components/ui-components/movie-list";
+import { dataContext } from "../context/dataContext";
+
 const Bookmarked = () => {
-  return <></>;
+  const { series: data } = useContext(dataContext);
+
+  return (
+    <>
+      <Nav />
+      <main className="main-content">
+        <h1 className="title">Bookmarked Movies</h1>
+        <MovieList data={data} />
+      </main>
+    </>
+  );
 };
 
 export default Bookmarked;
--- pages/series.js
+++ pages/series.js
@@ -1,5 +1,15 @@
+import Nav from "../components/navigation";
+import MovieList from "../components/ui-components/movie-list";
+
 const Series = () => {
-  return <></>;
+  const { series: data } = useContext(dataContext);
+  return (
+    <>
+      <Nav />
+      <h1 className="title">Series</h1>
+      <MovieList data={data} />
+    </>
+  );
 };
 
 export default Series;
