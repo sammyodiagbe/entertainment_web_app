14e72ee6ba4b7e8b3139a835f2f6708b05fda8b5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-15 19:47:17 -0500

    workio

--- components/image.js
+++ components/image.js
@@ -1,22 +1,10 @@
 import Image from "next/image";
 
 const CustomImage = ({ path }) => {
-  let mediumImagePath;
-  const { small, large, medium } = path;
-  console.log(`${small}`);
+  const { large } = path;
   const largeImagePath = large.substring(1);
-  const smallImagePath = small.substring(1);
 
-  mediumImagePath = medium ? medium.substring(1) : null;
-
-  return (
-    <Image
-      src={smallImagePath}
-      width={150}
-      height={150}
-      srcSet={`${smallImagePath}365w, ${mediumImagePath}760w, ${largeImagePath} 1440w`}
-    />
-  );
+  return <Image src={largeImagePath} width={250} height={250} />;
 };
 
 export default CustomImage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import DataContextProvider from "../context/dataContext";
 import stripAndPopulateData from "../utils/data-strip";
+import "../styles/index.css";
 
 // const DataContextProvider = ({ children }) => {
 //   return <dataContext.Provider>{children}</dataContext.Provider>;
