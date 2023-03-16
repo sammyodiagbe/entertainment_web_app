efff3f85ab1ab934d46888121bf734979604484c
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-16 13:26:55 -0500

    working on stylling

--- components/search-container.js
+++ components/search-container.js
@@ -4,17 +4,19 @@ import IconSearch from "./icons/icon-search";
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  <div className="search-container">
-    <form className="form">
-      <IconSearch />
-      <input
-        type="text"
-        placeholder="Search for movies or TV series"
-        value={searchQuery}
-        onChange={(event) => setSearchQuery(event.target.value)}
-      />
-    </form>
-  </div>;
+  return (
+    <div className="search-container">
+      <form className="form">
+        <IconSearch />
+        <input
+          type="text"
+          placeholder="Search for movies or TV series"
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Search;
