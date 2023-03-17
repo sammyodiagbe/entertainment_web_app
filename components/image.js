60fdce110af730212daae123d5e5cdd08a34d771
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-17 16:41:30 -0500

    fixing bugs

--- components/image.js
+++ components/image.js
@@ -3,7 +3,14 @@ import Image from "next/image";
 const CustomImage = ({ path }) => {
   const largeImagePath = path.substring(1);
 
-  return <Image src={largeImagePath} width={250} height={250} />;
+  return (
+    <Image
+      src={largeImagePath}
+      width={250}
+      height={250}
+      alt={"Awesome movie"}
+    />
+  );
 };
 
 export default CustomImage;
--- components/ui-components/movie-list.js
+++ components/ui-components/movie-list.js
@@ -2,7 +2,7 @@ import Movie from "./treding-movie";
 
 const MovieList = ({ data }) => {
   const moviesStructure = data.map((movie, index) => {
-    return <Movie movie={movie} />;
+    return <Movie movie={movie} key={index} />;
   });
   return <div className="movie-list-container">{moviesStructure}</div>;
 };
